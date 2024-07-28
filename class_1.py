665c506859e3bdcbe553719e05312c12c7926684
daniellemaxwell <danielle@rotational.io>
2024-07-28 16:21:30 -0400

    Print each sport

--- class_1.py
+++ class_1.py
@@ -2,7 +2,9 @@ list1 = [1, 2, 3, 4, 5]
 list2 = list1
 sports = ['soccer', 'basketball', 'tennis', 'baseball']
 
+for sport in sports:
+  print(sport)
+
 print('list 1', list1)
 print('list 2', list2)
-print('sports', sports)
 
