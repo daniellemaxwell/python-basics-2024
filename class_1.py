789ab17f2f2e7249b2723615822b899af6ee3267
daniellemaxwell <danielle@rotational.io>
2024-07-28 15:51:09 -0400

    Add colors list

--- class_1.py
+++ class_1.py
@@ -1,6 +1,8 @@
 list1 = [1, 2, 3, 4, 5]
 list2 = list1
+sports = ['soccer', 'basketball', 'football', 'tennis', 'baseball']
 
 print('list 1', list1)
 print('list 2', list2)
+print('sports', sports)
 
