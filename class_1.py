11683b42869a6d9c8548e6605b45ead284c0a2ce
daniellemaxwell <danielle@rotational.io>
2024-07-28 16:02:02 -0400

    Remove football

--- class_1.py
+++ class_1.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3, 4, 5]
 list2 = list1
-sports = ['soccer', 'basketball', 'football', 'tennis', 'baseball']
+sports = ['soccer', 'basketball', 'tennis', 'baseball']
 
 print('list 1', list1)
 print('list 2', list2)
